0efcff182ca889caa74edc8a75d1004b9d760ee6
anasfaisalamri <anasfaisalamri@gmail.com>
2023-08-22 23:05:51 +0700

    section gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ function disableScroll() {
 function enableScroll() {
   window.onscroll = function () {};
   rootElement.style.scrollBehavior = "smooth";
-  // localStorage.setItem("opened", "true");
+  localStorage.setItem("opened", "true");
 }
 
 if (!localStorage.getItem("opened")) {
